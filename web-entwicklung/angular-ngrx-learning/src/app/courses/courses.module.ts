303d4e1dc8fd4db16d2286350322ddb8a36185d2
nal95 <alex.nguepi@yahoo.de>
2022-10-14 19:51:34 +0200

    ngrx data : Courses data service

--- web-entwicklung/angular-ngrx-learning/src/app/courses/courses.module.ts
+++ web-entwicklung/angular-ngrx-learning/src/app/courses/courses.module.ts
@@ -21,9 +21,10 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {RouterModule, Routes} from '@angular/router';
-import {EntityDefinitionService, EntityMetadataMap} from '@ngrx/data';
+import {EntityDataService, EntityDefinitionService, EntityMetadataMap} from '@ngrx/data';
 import {CourseEntityService} from './services/course-entity.service';
 import {CoursesResolver} from './services/courses.resolver';
+import {CoursesDataService} from './services/courses-data.service';
 
 
 export const coursesRoutes: Routes = [
@@ -81,11 +82,17 @@ const entityMetadata: EntityMetadataMap = {
   providers: [
     CoursesHttpService,
     CourseEntityService,
-    CoursesResolver
+    CoursesResolver,
+    CoursesDataService
   ]
 })
 export class CoursesModule {
-  constructor(private eds: EntityDefinitionService) {
+  constructor(
+    private eds: EntityDefinitionService,
+    private entityDataService: EntityDataService,
+    private courseDataService: CoursesDataService
+    ) {
     eds.registerMetadataMap(entityMetadata);
+    entityDataService.registerService('Course', courseDataService);
   }
 }

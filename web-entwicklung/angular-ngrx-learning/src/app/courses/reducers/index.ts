77d9b0f4de15b3cb1200c4450663c3985e216099
nal95 <alex.nguepi@yahoo.de>
2022-10-14 06:48:13 +0200

    ngrx reducer: CoursesState courses reducer impl

--- web-entwicklung/angular-ngrx-learning/src/app/courses/reducers/index.ts
+++ web-entwicklung/angular-ngrx-learning/src/app/courses/reducers/index.ts
@@ -1,5 +1,7 @@
 import {Course} from "../model/course";
 import {createEntityAdapter, EntityState} from "@ngrx/entity";
+import {createReducer, on} from "@ngrx/store";
+import {CourseAction} from "../action-types";
 
 export interface CoursesState extends EntityState<Course>{
 
@@ -8,3 +10,10 @@ export interface CoursesState extends EntityState<Course>{
 export const adapter = createEntityAdapter<Course>();
 
 export const initialCoursesState = adapter.getInitialState();
+
+export const coursesReducer = createReducer(
+  initialCoursesState,
+  on(CourseAction.allCoursesLoaded,
+    (state,action)=>adapter.addMany(action.courses,state)
+  )
+);
